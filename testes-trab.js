06ea9e692b2bf1ca38c456f1f674aeaa617b360c
Bruno Miranda <brunomirandad@gmail.com>
2017-12-30 16:43:29 -0200

    adicionado novos testes

--- testes-trab.js
+++ testes-trab.js
@@ -51,4 +51,61 @@ describe('Protector Demo App', function() {
         
         expect(elem.getText()).toBe('There is 1 error\nThere is no account registered for this email address.');
     });
+
+    it('should show searched item name', function() {
+        
+        browser.waitForAngularEnabled(false);
+        
+        browser.get('http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=&submit_search=');
+        
+        element(by.id('search_query_top')).sendKeys('T-shirts > Faded Short Sleeve T-shirts');
+
+        element(by.name('submit_search')).click();
+        
+        let elem = element(by.tagName('h1'));
+        
+        var until  =  protractor.ExpectedConditions;
+        browser.wait(until.presenceOf(elem), 10000, 'waiting...');
+        
+        expect(elem.getText()).toBe('SEARCH  "T-SHIRTS > FADED SHORT SLEEVE T-SHIRTS"\n1 result has been found.');
+    });
+
+    it('should show searched item image', function() {
+        
+        browser.waitForAngularEnabled(false);
+        
+        browser.get('http://automationpractice.com/index.php');
+        
+        element(by.id('search_query_top')).sendKeys('T-shirts > Faded Short Sleeve T-shirts');
+
+        element(by.name('submit_search')).click();
+        
+        let elem = element(by.css('.product_img_link'));
+        
+        var until  =  protractor.ExpectedConditions;
+        browser.wait(until.presenceOf(elem), 10000, 'waiting...');
+        
+        expect(elem.getAttribute('title')).toBe('Faded Short Sleeve T-shirts');
+    });
+    
+    it('should show create account error message', function() {
+        
+        browser.waitForAngularEnabled(false);
+        
+        browser.get('http://automationpractice.com/index.php?controller=authentication&back=my-account');
+        
+        element(by.id('SubmitCreate')).click();
+        
+        let elem = element(by.id('create_account_error'));
+        
+        var until  =  protractor.ExpectedConditions;
+        browser.wait(until.presenceOf(elem), 10000, 'waiting...');
+
+        var li = element(by.id('create_account_error')).element(by.tagName('ol')).element(by.tagName('li'));
+
+        browser.wait(until.presenceOf(li), 10000, 'waiting...');
+        
+        expect(li.getText()).toBe('Invalid email address.');
+    });
+    
 });
